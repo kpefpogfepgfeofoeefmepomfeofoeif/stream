b34b529137191df6dce34395c000ab48c73c6818
kpefpogfepgfeofoeefmepomfeofoeif <43859071+kpefpogfepgfeofoeefmepomfeofoeif@users.noreply.github.com>
2018-11-04 11:31:51 +0300

    Update bot-1.js

--- bot-1.js
+++ bot-1.js
@@ -3,12 +3,12 @@ const client = new Discord.Client();
 var prefix = "+";
 client.on('ready', () => {
    console.log(`----------------`);
-      console.log(`Cyhper Bot Script By : DREAM`);
+      console.log(`Cyhper Bot Script By : M3a4x.js`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : M3a4x.js ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(` `,"http://twitch.tv/M3a4x")
 client.user.setStatus("dnd")
 });
 
